1eb6d3474683017f917c2539315ec7e16cd4b1b3
Junseong Jang <junsjang@amazon.com>
2022-07-12 23:33:51 +0900

    Fixed an error while updating a ranking.
    Removed item will remove the ranking entry.
    It can handle without error when there is no 'Score' attribute in the item.

--- Lambda/Scoring.py
+++ Lambda/Scoring.py
@@ -9,21 +9,13 @@ redis = redis.Redis(host=endpoint, port=6379, db=0)
 
 def handler(event, context):
     for record in event['Records']:
-        if record['eventName'] == "REMOVE":
-            continue
-        
         print(record)
-        playerName = record['dynamodb']['NewImage']['PlayerName']['S']
-        
-        if 'OldImage' in record['dynamodb'].keys():
-            oldWin = int(record['dynamodb']['OldImage']['Win']['N'])
-            oldLose = int(record['dynamodb']['OldImage']['Lose']['N'])
-            
-            olddata = { "PlayerName" : str(playerName), "Win" : int(oldWin), "Lose" : int(oldLose)}
-            old_json = json.dumps(olddata)
-            redis.zrem('Rating', old_json)
-        
-        newScore = int(record['dynamodb']['NewImage']['Score']['N'])
-        redis.zadd('Rating', str(playerName), float(newScore))
+        playerName = record['dynamodb']['Keys']['PlayerName']['S']
+
+        if record['eventName'] == "REMOVE":
+            redis.zrem('Rating', playerName)
+        elif 'Score' in record['dynamodb']['NewImage']:
+            newScore = int(record['dynamodb']['NewImage']['Score']['N'])
+            redis.zadd('Rating', { playerName: newScore })
 
     return "OK" 
